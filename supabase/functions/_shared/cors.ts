79917f6c3a457c7317a902d6c7b93732e27e675b
JeanVitorio <jeangalilei@gmail.com>
2025-09-10 10:42:55 -0300

    teste URL

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 // supabase/functions/_shared/cors.ts
-const allowedOrigins = Deno.env.get('ENV') === 'development' ? '*' : 'https://zailonsoft.com.br';
+const allowedOrigins = Deno.env.get('ENV') === 'development' ? '*' : 'www.zailonsoft.com.br';
 export const corsHeaders = {
   'Access-Control-Allow-Origin': allowedOrigins,
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
